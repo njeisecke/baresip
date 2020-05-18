4cd075cc790a087c45acf9f5385f60f579b18349
Nils Jeisecke <nils.jeisecke@saltation.com>
2020-05-18 17:02:14 +0200

    handle rport 'received' ip address in SDP
    This needs commit 2654229cd9f05f8610b5789443caefc227fa0d9e (Use 'rport'
    and 'received' parameters to adjust 'Contact' header according to RFC
    3581) in libre.

--- src/core.h
+++ src/core.h
@@ -245,6 +245,8 @@ const struct sa *reg_laddr(const struct reg *reg);
 const struct sa *reg_paddr(const struct reg *reg);
 void reg_set_custom_hdrs(struct reg *reg, const struct list *hdrs);
 
+const struct sa *reg_raddr(const struct reg *reg);
+
 /*
  * RTP Stats
  */
