8c08e84c819e7944010234210627582025e44f9d
Nils Jeisecke <nils.jeisecke@saltation.com>
2020-05-18 17:02:14 +0200

    handle rport 'received' ip address in SDP
    This needs commit e92d93ff8b3564b2bb6a0df86c1e71f8f5c83d7e in libre.

--- src/ua.c
+++ src/ua.c
@@ -1775,6 +1775,23 @@ const char *ua_cuser(const struct ua *ua)
 	return ua->cuser;
 }
 
+const struct sa *ua_raddr(const struct ua *ua)
+{
+	struct le *le;
+	int i;
+
+	if (!ua)
+		return NULL;
+
+	for (le = ua->regl.head, i=0; le; le = le->next, i++) {
+		struct reg *reg = le->data;
+		const struct sa *raddr = reg_raddr(reg);
+		if (raddr)
+			return raddr;
+	}
+
+	return NULL;
+}
 
 /**
  * Get the local contact username
